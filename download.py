f46fc418f7ea3df37c4cce673704f31a4d59017c
Stéphan Mestach <smestach@bluesquare.org>
2022-12-06 18:31:48 +0100

    More data

--- download.py
+++ download.py
@@ -2,12 +2,16 @@
 
 import os
 
-countries = ["be","fr"]
-
+countries = ["be","fr","de","ch","at","ba","bg","es","gr","nl"]
 months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
 years = ["2015","2016","2017","2018","2019","2020","2021","2022"]
 
 for country in countries:
     for year in years:
         for month in months:
-            os.system(f"wget https://www.energy-charts.info/charts/power/data/{country}/month_{year}_{month}.json -O ./data/{country}/month_{year}_{month}.json")
+            country_dir = f"./data/{country}"
+            if not os.path.exists(country_dir):
+                os.mkdir(country_dir)
+            output_file = f"{country_dir}/month_{year}_{month}.json"
+            if not os.path.exists(output_file):
+                os.system(f"wget https://www.energy-charts.info/charts/power/data/{country}/month_{year}_{month}.json -O {output_file}")
